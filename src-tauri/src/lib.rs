24b750daeb859706f5f87a5e2ef4eb8b6cc74fcb
sLuCHa <142535358+sLuCHaa@users.noreply.github.com>
2026-07-06 13:16:16 +0200

    feat(updater): add Tauri updater + process plugins pointed at GitHub

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -391,6 +391,8 @@ pub fn run() {
         ))
         .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_clipboard_manager::init())
         .plugin(
             tauri::plugin::Builder::<tauri::Wry>::new("window-chrome")
